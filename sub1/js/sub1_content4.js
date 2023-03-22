467fea49039d10e6844a5688e64caefec4ca45aa
parkyangji <dhsfid02@gmail.com>
2023-03-22 22:36:41 +0900

    sub1_4

--- sub1/js/sub1_content4.js
+++ sub1/js/sub1_content4.js
@@ -1,9 +1,7 @@
-$('.tabs .tab').eq(0).addClass('current');
-
-var history1 = $('.cont').eq(0).offset().top-72; //1243
-var history2 = $('.cont').eq(1).offset().top-72; //2132
-var history3 = $('.cont').eq(2).offset().top-72; //3331
-var history4 = $('.cont').eq(3).offset().top-72; //4203
+var history1 = $('.cont').eq(0).offset().top-72;
+var history2 = $('.cont').eq(1).offset().top-72;
+var history3 = $('.cont').eq(2).offset().top-72;
+var history4 = $('.cont').eq(3).offset().top-72;
 
 
 //탭 클릭시
@@ -32,29 +30,35 @@ var navTop = $('.tabs').offset().top;
 
 $(window).on('scroll', function() {
     var scroll = $(window).scrollTop();
+    var scrollEnd = $(document).height() - $(window).height()
 
     //네비 고정
     if(navTop<scroll){
         $('.tabs').addClass('navOn');
         $('#headerArea').fadeOut('slow');
-        $('#history1').css('margin-top','352px')
+        $('#history1').css('margin-top','352px');
 
     }else {
         $('.tabs').removeClass('navOn');
         $('#headerArea').fadeIn('slow');
-        $('#history1').css('margin-top','0')
+        $('#history1').css('margin-top','0');
     }
 
     //탭 스크롤시
     $('.tab').removeClass('current');
+    $('.cont').find('h3').removeClass('textFixed');
 
     if (scroll>=history1 && scroll<history2) {
         $('.tab1').addClass('current');
+        $('.cont').eq(0).find('h3').addClass('textFixed');
     } else if (scroll>=history2 && scroll<history3) {
         $('.tab2').addClass('current');
-    } else if (scroll>=history3 && scroll<history4-100) {
+        $('.cont').eq(1).find('h3').addClass('textFixed');
+    } else if (scroll>=history3 && scroll<history4) {
         $('.tab3').addClass('current');
-    } else if (scroll>=history4-100) {
+        $('.cont').eq(2).find('h3').addClass('textFixed');
+    } else if (scroll>=history4) {
         $('.tab4').addClass('current');
+        $('.cont').eq(3).find('h3').addClass('textFixed');
     }
 })
