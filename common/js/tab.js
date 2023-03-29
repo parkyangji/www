84c24a4c6899a7e865859c158a12817669637c72
parkyangji <dhsfid02@gmail.com>
2023-03-29 14:19:33 +0900

    tab js

--- common/js/tab.js
+++ common/js/tab.js
@@ -1,3 +1,26 @@
+const tabArr = document.querySelectorAll('.tab');
+const contArr = document.querySelectorAll('.cont');
+
+tabArr[0].classList.add('current');
+contArr[0].style.display = "block";
+
+tabArr.forEach(function(a, i ) {
+    a.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        for (let j=0; j<tabArr.length; j++) {
+            contArr[j].style.display = "none";
+            tabArr[j].classList.remove('current');
+        }
+
+        contArr[i].style.display = "block";
+        tabArr[i].classList.add('current');
+    })
+})
+
+
+/* JQUERY 버전
+
 $('.cont').eq(0).show();
 $('.tabs .tab').eq(0).addClass('current');
       
@@ -13,6 +36,8 @@ $('.tabs .tab').click(function(e){
         $(this).addClass('current');
 
     });
+
+*/
 
 
     
