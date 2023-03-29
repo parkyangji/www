c4c748f6e606e15143576cc11a45686d501307ad
parkyangji <dhsfid02@gmail.com>
2023-03-29 17:09:25 +0900

    sub1_4 ing

--- sub1/js/sub1_content4.js
+++ sub1/js/sub1_content4.js
@@ -1,3 +1,100 @@
+// 스크롤시
+const tabs = document.querySelector('.tabs');
+const tabArr = document.querySelectorAll('.tab');
+const contArr = document.querySelectorAll('.cont');
+
+let navTop = tabs.offsetTop;
+let history1 = contArr[0].offsetTop-72;
+let history2 = contArr[1].offsetTop-72;
+let history3 = contArr[2].offsetTop-72;
+let history4 = contArr[3].offsetTop-72;
+
+window.addEventListener('scroll', function() {
+    let scroll = window.scrollY;
+
+    //네비 고정
+
+    if(navTop-200<scroll) {
+        headerArea.style.display = 'none';
+        tabs.classList.add('navOn');
+        contArr[0].style.marginTop = '352px'
+
+    } else {
+        headerArea.style.display = 'block';
+        tabs.classList.remove('navOn');
+        contArr[0].style.marginTop = '0'
+    }
+
+
+    //탭 스크롤시
+
+
+    if (scroll>=history1 && scroll<history2) {
+        tabArr[0].classList.add('current');
+        contArr[0].children[0].children[0].classList.add('textFixed');
+    } else if (scroll>=history2 && scroll<history3) {
+        tabArr[1].classList.add('current');
+        contArr[1].children[0].children[0].classList.add('textFixed');
+    } else if (scroll>=history3 && scroll<history4) {
+        tabArr[2].classList.add('current');
+        contArr[2].children[0].children[0].classList.add('textFixed');
+    } else if (scroll>=history4) {
+        tabArr[3].classList.add('current');
+        contArr[3].children[0].children[0].classList.add('textFixed');
+    } else if (scroll<history1) {
+        tabArr[0].classList.add('current');
+    }
+
+
+})
+
+/* JQUERY 버전 
+
+var navTop = $('.tabs').offset().top;
+
+$(window).on('scroll', function() {
+    var scroll = $(window).scrollTop();
+
+
+    if(navTop-200<scroll){
+        $('.tabs').addClass('navOn');
+        $('#headerArea').fadeOut('slow');
+        $('#history1').css('margin-top','352px');
+
+    }else {
+        $('.tabs').removeClass('navOn');
+        $('#headerArea').fadeIn('slow');
+        $('#history1').css('margin-top','0');
+    }
+
+
+    $('.tab').removeClass('current');
+    $('.cont').find('h3').removeClass('textFixed');
+
+    if (scroll>=history1 && scroll<history2) {
+        $('.tab1').addClass('current');
+        $('.cont').eq(0).find('h3').addClass('textFixed');
+    } else if (scroll>=history2 && scroll<history3) {
+        $('.tab2').addClass('current');
+        $('.cont').eq(1).find('h3').addClass('textFixed');
+    } else if (scroll>=history3 && scroll<history4) {
+        $('.tab3').addClass('current');
+        $('.cont').eq(2).find('h3').addClass('textFixed');
+    } else if (scroll>=history4) {
+        $('.tab4').addClass('current');
+        $('.cont').eq(3).find('h3').addClass('textFixed');
+    } else if (scroll<history1) {
+        $('.tab1').addClass('current');
+    }
+})
+
+*/
+
+
+
+
+/* JQUERY 버전 
+
 var history1 = $('.cont').eq(0).offset().top-72;
 var history2 = $('.cont').eq(1).offset().top-72;
 var history3 = $('.cont').eq(2).offset().top-72;
@@ -25,43 +122,4 @@ $('.tab').click(function(e){
 $("html,body").stop().animate({"scrollTop":value},1000);
 });
 
-// 스크롤시
-var navTop = $('.tabs').offset().top;
-
-$(window).on('scroll', function() {
-    var scroll = $(window).scrollTop();
-    var scrollEnd = $(document).height() - $(window).height()
-
-
-    //네비 고정
-    if(navTop-200<scroll){
-        $('.tabs').addClass('navOn');
-        $('#headerArea').fadeOut('slow');
-        $('#history1').css('margin-top','352px');
-
-    }else {
-        $('.tabs').removeClass('navOn');
-        $('#headerArea').fadeIn('slow');
-        $('#history1').css('margin-top','0');
-    }
-
-    //탭 스크롤시
-    $('.tab').removeClass('current');
-    $('.cont').find('h3').removeClass('textFixed');
-
-    if (scroll>=history1 && scroll<history2) {
-        $('.tab1').addClass('current');
-        $('.cont').eq(0).find('h3').addClass('textFixed');
-    } else if (scroll>=history2 && scroll<history3) {
-        $('.tab2').addClass('current');
-        $('.cont').eq(1).find('h3').addClass('textFixed');
-    } else if (scroll>=history3 && scroll<history4) {
-        $('.tab3').addClass('current');
-        $('.cont').eq(2).find('h3').addClass('textFixed');
-    } else if (scroll>=history4) {
-        $('.tab4').addClass('current');
-        $('.cont').eq(3).find('h3').addClass('textFixed');
-    } else if (scroll<history1) {
-        $('.tab1').addClass('current');
-    }
-})
+*/
