dbbd710ed5384df8acc5bca9c8d8e5626faae8c4
parkyangji <dhsfid02@gmail.com>
2023-04-02 17:15:33 +0900

    sub4_2

--- sub4/js/sub4_content2.js
+++ sub4/js/sub4_content2.js
@@ -1,25 +1,71 @@
+const chapterAll = document.querySelectorAll('.chapter');
+const titleArr = Object.values(chapterAll).map(function(el){
+    return el.previousElementSibling
+})
+const all_btn = document.querySelector('.all');
+
+titleArr.forEach((a, i ) => {
+    a.addEventListener('click', (e) => {
+        const target = e.target;
+        e.preventDefault();
+
+        chapterAll[i].style.display = (chapterAll[i].style.display == 'block') ? 'none' : 'block';
+
+        if (target.className == 'rotate') {
+            target.classList.remove('rotate');
+        } else {
+            target.classList.add('rotate');
+        }
+        
+    })
+})
+
+let all = false;
+
+all_btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    if (all) {
+        for (let i=0; i<chapterAll.length; i++) {
+            chapterAll[i].style.display = 'none';
+            titleArr[i].classList.remove('rotate');
+        }
+        all_btn.innerHTML = '모두 열기';
+        all = false;
+    } else {
+        for (let i=0; i<chapterAll.length; i++) {
+            chapterAll[i].style.display = 'block';
+            titleArr[i].classList.add('rotate');
+        }
+        all_btn.innerHTML = '모두 닫기';
+        all = true;
+    }
+})
+
+
+
+/* JQUERY 버전
+
 // 각각 열기
-$('.chapter a').click(function(e){ 
+$('.ethics_conduct li a').click(function(e){ 
     e.preventDefault(); 
-    var chapterOpen = $(this).parent().siblings('dd');
+    var chapter = $(this).siblings();
 
     $(this).toggleClass('rotate');
-    chapterOpen.slideToggle('linear');
+    chapter.slideToggle('linear');
 });
 
 // 모두 열기
-var chapter = $('.chapter a')
-var chapterAll = $('.chapter').siblings('dd');
-var allText = $('.all');
-
 $('.all').toggle(function(e){
     e.preventDefault();
-    chapter.addClass('rotate');
-    chapterAll.slideDown('linear');
-    allText.text('모두 닫기');
+    $('.ethics_conduct li a').addClass('rotate');
+    $('.chapter').slideDown('linear');
+    $(this).text('모두 닫기');
 }, function(e){
     e.preventDefault();
-    chapter.removeClass('rotate');
-    chapterAll.slideUp('linear');
-    allText.text('모두 열기');
+    $('.ethics_conduct li a').removeClass('rotate');
+    $('.chapter').slideUp('linear');
+    $(this).text('모두 열기');
 })
+
+*/
